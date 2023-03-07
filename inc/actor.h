774af1ef9910ae8a0d46a4b7dab0f906fbd01e4b
Bozhi Bao <baobozhi@outlook.com>
2023-03-07 18:30:05 +0100

    [TASK] Implement actor

--- inc/actor.h
+++ inc/actor.h
@@ -1,7 +1,17 @@
-#include <thread>
 #include "safe_queue.h"
+#include <thread>
 
 class Actor
 {
 public:
+    Actor();
+    ~Actor();
+    void start();
+    void stop();
+    void set_affinity();
+    virtual void run() = 0;
+
+private:
+    safe_queue<int> q;
+    std::thread t_;
 };
--- src/actor.cpp
+++ src/actor.cpp
@@ -1,1 +1,15 @@
 #include "actor.h"
+
+Actor::Actor(){};
+
+Actor::~Actor(){};
+
+void Actor::start()
+{
+    t_ = std::thread(&Actor::run, this);
+}
+
+void Actor::stop()
+{
+    t_.join();
+}
